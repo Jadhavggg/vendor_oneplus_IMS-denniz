6c9f742e0cfdd842614fe09d5bcd61753b45d47b
lahaina <nattolecats@gmail.com>
2023-06-13 22:46:34 +0900

    IMS-denniz: Remove separated ims blobs * We needs stock one.

--- mtk-ims.mk
+++ mtk-ims.mk
@@ -14,13 +14,3 @@ PRODUCT_BOOT_JARS += \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common
-
-# Radio IMS Jars
-PRODUCT_PACKAGES += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
